4ff5fc854b3684c79818f5fc8fff9e0607b4a6eb
Mikko Uuksulainen <mikko.uuksulainen@gmail.com>
2017-09-26 11:07:06 +0300

    Add project files.

---
+++ PeliplaneettaParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PeliplaneettaParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
